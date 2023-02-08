e068765191e63c9ef94a27fc3096a25c088746f9
Grzegorz Pałka <grzegorz.palka@redigames.com>
2023-02-08 18:00:51 +0100

    Movemnt

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Jump : Unit
+{
+    
+    public void Jumping()
+    {
+    }
+
+    protected override void Definition()
+    {
+    }
+}
